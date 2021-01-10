639a15a9d6be11c3b943d1fe6e817bee4382fc65
ArttuOll <arttuoll.tech@tutanota.com>
2021-01-10 15:27:33 +0200

    Tyylittele ja asettele makrorivi

--- src/elements/body.js
+++ src/elements/body.js
@@ -1,57 +1,58 @@
-import ComponentCard from "./component_card";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import ComponentCard from "./component_card";
+import {BigLabel} from "./utils/styled_texts";
 
 const Body = () => {
   return (
-    <div style={{ backgroundColor:"#F4F4F4" }}>
+    <div>
       <MacroComponentsRow/>
     </div>
   );
 };
 
 const MacroComponentsRow = () => {
+  const componentCards = [{ title: "Energia",
+                            unit: "Kilokaloria",
+                            value: "60",
+                            valueSubtitle: "X % suosituksesta"
+                          },
+                          { title: "Hiilihydraatit",
+                            unit: "g / 100 g",
+                            value: "20",
+                            valueSubtitle: "X % suosituksesta"
+                          },
+                          { title: "Proteiini",
+                            unit: "g / 100 g",
+                            value: "45",
+                            valueSubtitle: "X % suosituksesta"
+                          },
+                          { title: "Rasva",
+                            unit: "g / 100 g",
+                            value: "4",
+                            valueSubtitle: "X % suosituksesta"
+                         }];
+
   return (
-    <div>
-      <h1>Energiaravintoaineet</h1>
-      <Container>
-        <Row>
-          <Col md>
-            <ComponentCard
-              title="Energia"
-              unit="Kilokaloria"
-              value="60"
-              valueSubtitle="X % suosituksesta"
-            />
-          </Col>
-          <Col md>
-            <ComponentCard
-              title="Hiilihydraatit"
-              unit="g/100 g"
-              value="20"
-              valueSubtitle="X % suosituksesta"
-            />
-          </Col>
-          <Col md>
-            <ComponentCard
-              title="Proteiini"
-              unit="g/100 g"
-              value="90"
-              valueSubtitle="X % suosituksesta"
-            />
-          </Col>
-          <Col md>
-            <ComponentCard
-              title="Rasva"
-              unit="g/100 g"
-              value="2"
-              valueSubtitle="X % suosituksesta"
-            />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container>
+      <BigLabel className="mb-3 text-break">
+        Energiaravintoaineet
+      </BigLabel>
+      <Row xs={1} sm={2} md={4} style={{ backgroundColor:"#F4F4F4" }}>
+        {componentCards.map((componentCard, index) => (
+            <Col className="d-flex justify-content-center">
+              <ComponentCard
+                key={index}
+                title={componentCard.title}
+                unit={componentCard.unit}
+                value={componentCard.value}
+                valueSubtitle={componentCard.valueSubtitle}
+              />
+            </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
---
+++ src/elements/utils/styled_texts.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const HiglightedValue = styled.h1`
+  font-family: "Trebuchet MS";
+  color:#65BA41
+`;
+
+const BigLabel = styled.h2`
+  font-family: "Verdana";
+`;
+
+const TextAlert = styled.h6`
+  color: #EC4C4C;
+`;
+
+const TextOk = styled.h6`
+  color: #65BA41;
+`;
+
+const TextWarning = styled.h6`
+  color: #BAB43C;
+`;
+
+export { HiglightedValue, BigLabel, TextAlert, TextOk, TextWarning };
