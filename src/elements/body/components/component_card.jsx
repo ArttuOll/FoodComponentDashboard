1d741464fec821bd2804af7e0d5bd27928540e2f
ArttuOll <arttuoll.tech@tutanota.com>
2021-02-25 14:30:20 +0200

    Korjaa yksiköiden nimien kielioppi

--- src/elements/body/components/component_card.jsx
+++ src/elements/body/components/component_card.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Proptypes from "prop-types";
-import { HiglightedValue, TextOk } from "elements/utils/styled_texts";
+import { HiglightedValue } from "elements/utils/styled_texts";
 
 const ComponentCard = (props) => {
-  const { title, unit, value, valueSubtitle } = props;
+  const { title, unit, value } = props;
   return (
     <Card className="text-center shadow-sm my-2" style={{ width: "25rem" }}>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{unit}</Card.Subtitle>
         <Card.Title>
           <HiglightedValue>{value}</HiglightedValue>
         </Card.Title>
-        <Card.Subtitle className="mb-2">
-          <TextOk>{valueSubtitle}</TextOk>
-        </Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted">{`${unit}a`}</Card.Subtitle>
       </Card.Body>
     </Card>
   );
@@ -25,7 +22,6 @@ ComponentCard.propTypes = {
   title: Proptypes.string.isRequired,
   unit: Proptypes.string.isRequired,
   value: Proptypes.number.isRequired,
-  valueSubtitle: Proptypes.string.isRequired,
 };
 
 export default ComponentCard;
