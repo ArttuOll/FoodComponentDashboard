add5583b88bd3a9c81da07e1b78c9e60b4a17a0e
ArttuOll <arttuoll.tech@tutanota.com>
2021-07-04 12:51:00 +0300

    Convert makeApiRequest to curried form

--- src/elements/utils/network_utils.js
+++ src/elements/utils/network_utils.js
@@ -2,31 +2,33 @@ import { ACTIONS } from "elements/header/components/search_bar/search_bar_reduce
 
 const API_URL = process.env.REACT_APP_URL;
 
-const makeApiRequest = async (url, resultCallback, errorCallback) => {
-  try {
-    const response = await fetch(url);
-    const responseJson = await response.json();
-    const searchResults = responseJson.result;
-    resultCallback(searchResults);
-    errorCallback("");
-  } catch (error) {
-    errorCallback(`Error fetching search suggestions: ${error}`);
-  }
-};
+function makeGetRequest(url) {
+  return function then(resultCallback) {
+    return async function ifError(errorCallback) {
+      try {
+        const response = await fetch(url);
+        const responseJson = await response.json();
+        const searchResults = responseJson.result;
+        resultCallback(searchResults);
+        errorCallback("");
+      } catch (error) {
+        errorCallback(`Error fetching search suggestions: ${error}`);
+      }
+    };
+  };
+}
 
 const fetchFoodNames = async (searchQuery, dispatch, setErrorMessage) => {
   const url = `${API_URL}/?q=${searchQuery}`;
   const resultCallback = (searchResults) => {
     dispatch({ type: ACTIONS.SET_SEARCH_RESULTS, payload: { searchResults } });
   };
-
-  makeApiRequest(url, resultCallback, setErrorMessage);
+  await makeGetRequest(url)(resultCallback)(setErrorMessage);
 };
 
 const fetchFoodCompositionData = async (foodId, foodDataCallback, setErrorMessage) => {
   const url = `${API_URL}/${foodId}`;
-  const resultCallback = foodDataCallback;
-  makeApiRequest(url, resultCallback, setErrorMessage);
+  makeGetRequest(url)(foodDataCallback)(setErrorMessage);
 };
 
 export { fetchFoodNames, fetchFoodCompositionData };
